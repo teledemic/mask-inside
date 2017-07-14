6fd4fdc82da41728fa79e0a251a0dc892d678bde
Carl Irving <carl@teledemic.com>
2017-07-14 13:37:42 -0400

    Initial video

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('video1') video1; 
+  @ViewChild('video2') video2; 
+  stream: MediaStream;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  async ionViewDidLoad() {
+    this.stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    this.video1.nativeElement.srcObject = this.stream;
+    this.video2.nativeElement.srcObject = this.stream;
   }
 
 }
