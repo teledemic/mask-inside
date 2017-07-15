9fd7ec1ef7d34529113a4a2745155988f23aa5c6
Carl Irving <carl@teledemic.com>
2017-07-15 11:37:07 -0400

    Add sounds Reconnect socket

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  yes = new Audio("assets/sound/yes.wav");
-  no = new Audio("assets/sound/no.mp3");
+  sounds = {
+    yes: new Audio("assets/sound/yes.mp3"),
+    no: new Audio("assets/sound/no.mp3"),
+    ding: new Audio("assets/sound/ding.mp3"),
+    bell: new Audio("assets/sound/bell.mp3"),
+  }
+  sock: WebSocket;
 
   constructor(public navCtrl: NavController) {
 
   }
 
   async ionViewDidLoad() {
-    this.yes.play();
-    this.no.play();
+    this.hookupSocket();
+  }
+
+  hookupSocket() {
+    this.sock = new WebSocket("ws://192.168.2.107:8080");
+    this.sock.addEventListener('open', () => {
+      this.sock.send('Hello Server!');
+    });
+    this.sock.addEventListener("close", () => {
+      setTimeout(() => {
+        this.hookupSocket();
+      }, 5000);
+    });
+    this.sock.addEventListener("message", message => {
+      if (this.sounds.hasOwnProperty(message.data)) {
+        this.sounds[message.data].play();
+      }
+    });
   }
 
 }
