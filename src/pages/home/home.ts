c7a088945f8c6abe9aeaab4e3eb7c43493419064
Carl Irving <carl@teledemic.com>
2017-07-15 13:48:54 -0400

    Switch to socket.io

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,43 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import * as socketio from "socket.io-client";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  message = "";
+  error = "";
   sounds = {
     yes: new Audio("assets/sound/yes.mp3"),
     no: new Audio("assets/sound/no.mp3"),
     ding: new Audio("assets/sound/ding.mp3"),
     bell: new Audio("assets/sound/bell.mp3"),
   }
-  sock: WebSocket;
+  sock: any;
 
   constructor(public navCtrl: NavController) {
-
   }
 
-  async ionViewDidLoad() {
+  ionViewDidLoad() {
     this.hookupSocket();
   }
 
   hookupSocket() {
-    this.sock = new WebSocket("ws://192.168.2.107:8080");
-    this.sock.addEventListener('open', () => {
-      this.sock.send('Hello Server!');
-    });
-    this.sock.addEventListener("close", () => {
-      setTimeout(() => {
-        this.hookupSocket();
-      }, 5000);
-    });
-    this.sock.addEventListener("message", message => {
-      if (this.sounds.hasOwnProperty(message.data)) {
-        this.sounds[message.data].play();
-      }
-    });
+    try {
+      this.error = "opening";
+      this.sock = socketio("192.168.2.107:8080");
+      this.sock.on("sound", msg => {
+        if (this.sounds.hasOwnProperty(msg)) {
+          this.sounds[msg].play();
+        }
+      });
+    } catch (err) {
+      this.message = err;
+    }
   }
 
 }
