fd425c3d709aa71f6723a9842b252dfa7f16a401
Carl Irving <carl@teledemic.com>
2017-07-15 11:56:01 -0400

    Add insomnia Change colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,26 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Insomnia } from '@ionic-native/insomnia';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private insomnia: Insomnia) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.hide();
       splashScreen.hide();
+      if (platform.is("cordova")) {
+        insomnia.keepAwake().catch(err => {
+          console.log(err);
+        });
+      }
     });
   }
 }
